432ccf51839d48f3acb8ba528c1191258b343247
xdankim <danykimy2208@gmail.com>
2026-05-23 16:09:06 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,27 +1,38 @@
-## What is this?
+## IJM (Import Java Mods)
 
-A file that read the jar mods to copy and import them into Minecraft Java folder `.minecraft/mods`
+Built a tool that imports Java Minecraft mods directly into the game without needing to open the mods folder to paste the mods.
 
 ## How to use it?
 
 1. Double click file you just downloaded and it show the notice about an error to create a folder. just click ok (ignore them)
-2. Copy the current path of location folder which all your mods in that folder. let say i save my mods at `C:\Users\{windowname}\Downloads`. so, just copy the location and paste it.
+2. Copy the current path of location folder which all your mods in that folder. let say i save my mods at `C:\Users\{USERPROFILE}\Downloads`. so, just copy the location and paste it.
 3. Copy your `C:\Users\...\.minecraft\mods` location.
 4. Choose your mods to copy and click ok
 
-- Mods located (default) : `C:\Users\{windowname}\Downloads`
-- Minecraft Folder : `C:\Users\{windowname}\AppData\Roaming\.minecraft\mods`;
+- Mods located (default) : `C:\Users\{USERPROFILE}\Downloads`
+- Minecraft Folder : `C:\Users\{USERPROFILE}\AppData\Roaming\.minecraft\mods`;
+
+## is this actually useful?
+
+if you are lazy to copy and paste or drag the mods into the mods folder you can use this instead. i made this tool for my friend which he didnt know about computer. To ease it, i used to spend a week to design the code. But if you are comfortable with manual setup or tradisional method, this tool would be not that crucial for you and it nothing wrong if you want to use it.
+
+if you got an error, you can remove `.path` folder at Download. Makesure the show hidden file/folder is checked.
+For legacy launcher, you should do this first!
+
+> For version 1.0.1
+Press `⊞` + `r` at the same time at keyboard and type `%appdata%\.tlauncher\legacy\Minecraft\game`. If you guys dont see a mods folder just create a new folder there.
+
+> For version 1.1
+You dont need to press `⊞` + `r` to know the location of game. Java already read where the path of legacy launcher stay.
 
 ## Other
 
-i made this project for newbie java players which they arent adapt with java environemnt.
-if you got an error, you can remove `.path` folder at Download. Makesure the show hidden file/folder is checked.
-For legacy launcher or any crack version, you should do this first!
-
-Press `⊞` + `r` at keyboard and type `%appdata%\.tlauncher\legacy\Minecraft\game`. If you guys dont see a mods folder just create a new folder there.
 i should focus more about to improve my light project to everyone especially Minecraft Java player. Hopefully you guys enjoy. (sorry for bad english cz i ignore the grammar)
 
 ## Download?
 
 > https://xdankim.github.io/ijm/
 
+## Check also this tool
+
+> https://github.com/xdankim/simple-import-java-mods
--- src/App.java
+++ src/App.java
@@ -8,18 +8,133 @@ import java.util.ArrayList;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 
+
 public class App {
     public static void main(String[] args) {
-        new Setup();   
+        new Setup_();   
     }
 }
 
+class Setup_ extends FileSystem {
+    public Setup_(){
+        checkfolder();
+
+        if (isLock) return;
+        else {
+            try {
+                Files.list(Paths.get(Files.readString(readFile(this.PathSourceTXT))))
+                    .filter(p -> p.toString().endsWith(".jar"))
+                    .forEach(files -> {
+                        this.FileName.add(files.getFileName().toString()); // pathfilelist
+                        this.PathFileLocation.add(files.toString());
+                    });
+
+                String[] option = FileName.toArray(new String[0]);
+
+                JComboBox<String> comboBox = new JComboBox<>(option);
+
+                int result = JOptionPane.showConfirmDialog(null, comboBox, "Available Mods", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+
+                if (result == JOptionPane.OK_OPTION){
+                    Path directory = Paths.get(Files.readString(readFile(this.PathTargetTXT)));
+                    Files.createDirectories(directory);
+
+                    Path target = directory.resolve(FileName.get(comboBox.getSelectedIndex()));
+                    
+                    Files.copy(readFile(PathFileLocation.get(comboBox.getSelectedIndex())), target);
+
+                    JOptionPane.showOptionDialog(null, "The Mods just imported to Minecraft", "Successfully", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+                } else {
+                    JOptionPane.showOptionDialog(null, "Import Cancelled", "Cancelled", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+                }
+                
+            } 
+            catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e, "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+    
+    public void checkfolder(){
+        try {
+            Path minecraft_folder = Path.of(minecraftdir);
+            Path legacy_folder = Path.of(legacydir);
+
+            if (Files.exists(minecraft_folder) && Files.exists(legacy_folder)) {
+                JOptionPane.showInputDialog("Java cant read .minecraft and .tlauncher because you\nhave two folders of minecraft launcher. to avoid this happen, \n\nPlease report to me in github for releasing new version.", "https://github.com/xdankim/ijm/issues");
+                isLock = true;
+            }
+            else if (Files.exists(minecraft_folder)) isOriginal(true);
+            else if (Files.exists(legacy_folder)) isOriginal(false);
+            else {
+                JOptionPane.showMessageDialog(null,
+                     "Java cant read the folder of .minecraft or .tlauncher because of:\n\n" 
+                     + "1. You aren't install the original of minecraft\n"
+                     + "2. You use tlauncher because this version only available for legacy launcher\n"
+                     + "3. Different location. Java only read local disk which C:\\User\\...",
+                     "Sorry, error was occured", 
+                     JOptionPane.ERROR_MESSAGE, 
+                     null);
+            }
+        } catch (Exception e) {}
+    }
+
+    public void isOriginal(Boolean bool){
+        if (bool) build(pathdefault, minecraftdir); // original
+        else build(pathdefault, insidelegacydir); // legacy
+    }
+
+    /**
+     * 
+     * @param Source
+     * Folder sekarang dimana mods berada (boleh tukar)
+     * @param Target
+     * Folder untuk target
+     */
+    public void build(String Source, String Target){
+        try {
+            Path folder = Path.of(this.PathFolder);
+
+            if (Files.exists(folder)) return;
+            else {
+                JOptionPane.showMessageDialog(null, "Folder is missing at "+ PathFolder +",\nYou should create that folder. Just click ok", "Warning", JOptionPane.WARNING_MESSAGE, null);
+
+                Files.createDirectories(Path.of(this.PathFolder));
+
+                createFile(this.PathSourceTXT);
+                createFile(this.PathTargetTXT);
+
+                String source = JOptionPane.showInputDialog("Insert your current location of mods", Source);
+
+                WriteFile(this.PathSourceTXT, source);
+
+                String target = JOptionPane.showInputDialog("Insert your .minecraft/mods location", Target + "\\mods");
+
+                WriteFile(this.PathTargetTXT, target);
+
+                JOptionPane.showMessageDialog(null, "Set up is finished. Lets start", "Successfully", JOptionPane.INFORMATION_MESSAGE);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
+
+
 class FileSystem {
 
+    /**Nama File itu sendiri */
     public ArrayList<String> FileName = new ArrayList<>();
+    /** Lokasi file berada */
     public ArrayList<String> PathFileLocation = new ArrayList<>();
 
-    protected final String home = System.getProperty("user.home");
+    protected final static String home = System.getProperty("user.home");
+    public final static String minecraftdir = home + "\\AppData\\Roaming\\.minecraft";
+    public final static String legacydir = home + "\\AppData\\Roaming\\.tlauncher\\legacy";
+    public final static String insidelegacydir = legacydir + "\\Minecraft\\game";
+    public static Boolean isLock = false;
+
     protected final String pathdefault = home + "\\Downloads";
     protected final String pathminecraft = home + "\\AppData\\Roaming\\.minecraft\\mods";
     protected final String PathFolder = home + "\\Downloads\\.path";
@@ -48,71 +163,4 @@ class FileSystem {
         return Path.of(FileName);
     }
 
-}
-
-class Setup extends FileSystem {
-    public Setup(){
-        this.checkFolder();
-
-        try {
-            Files.list(Paths.get(Files.readString(readFile(this.PathSourceTXT))))
-                .filter(p -> p.toString().endsWith(".jar"))
-                .forEach(files -> {
-                    this.FileName.add(files.getFileName().toString()); // pathfilelist
-                    this.PathFileLocation.add(files.toString());
-                });
-
-            String[] option = FileName.toArray(new String[0]);
-
-            JComboBox<String> comboBox = new JComboBox<>(option);
-
-            int result = JOptionPane.showConfirmDialog(null, comboBox, "Available Mods", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
-
-
-            if (result == JOptionPane.OK_OPTION){
-                Path directory = Paths.get(Files.readString(readFile(this.PathTargetTXT)));
-                Files.createDirectories(directory);
-
-                Path target = directory.resolve(FileName.get(comboBox.getSelectedIndex()));
-                
-                Files.copy(readFile(PathFileLocation.get(comboBox.getSelectedIndex())), target);
-
-                JOptionPane.showOptionDialog(null, "The Mods just imported to Minecraft", "Successfully", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
-            } else {
-                JOptionPane.showOptionDialog(null, "Import Cancelled", "Cancelled", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
-            }
-             
-        } 
-        catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e, "Error", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    public void checkFolder(){
-        try {
-            Path folder = Path.of(this.PathFolder);
-
-            if (Files.exists(folder)) return;
-            else {
-                JOptionPane.showMessageDialog(null, "Folder is missing at "+ PathFolder +",\nYou should create that folder. Just click ok", "Error Message", JOptionPane.ERROR_MESSAGE, null);
-
-                Files.createDirectories(Path.of(this.PathFolder));
-
-                createFile(this.PathSourceTXT);
-                createFile(this.PathTargetTXT);
-
-                String source = JOptionPane.showInputDialog("Insert your current location of mods", this.pathdefault);
-
-                WriteFile(this.PathSourceTXT, source);
-
-                String target = JOptionPane.showInputDialog("Insert your .minecraft/mods location", this.pathminecraft);
-
-                WriteFile(this.PathTargetTXT, target);
-
-                JOptionPane.showMessageDialog(null, "Set up is finished. Lets start", "Successfully", JOptionPane.INFORMATION_MESSAGE);
-            }
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e, "Error", JOptionPane.ERROR_MESSAGE);
-        }
-    }
 }
